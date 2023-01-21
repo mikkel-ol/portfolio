f58276fd372a7ee9eeccc6bc500ecee7bb894917
Mikkel O. Laursen <m@ikkel.dk>
2023-01-21 17:17:28 +0100

    Add Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikSpeakInline } from "qwik-speak/inline";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      qwikSpeakInline({
+        basePath: "./",
+        assetsPath: "public/i18n",
+        supportedLangs: ["en-US", "da-DK"],
+        defaultLang: "da-DK",
+      }),
+      tsconfigPaths(),
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
