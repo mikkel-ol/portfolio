e45a783d3e1c7d9cd8cc0b26bf29c8a70b5a83a3
mikkel-ol <m@ikkel.dk>
2024-01-03 16:12:13 +0100

    Add bun vite react template

--- src/components/lang-select/lang-select.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
-
-import styles from "./lang-select.scss?inline";
-
-export default component$(() => {
-  useStylesScoped$(styles);
-
-  return <div class="container"></div>;
-});
